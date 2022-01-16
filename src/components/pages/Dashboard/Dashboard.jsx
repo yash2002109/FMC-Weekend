1515ceb8b8a1a382718b1845c946c7673b379e5a
yash2002109 <yash2002109@gmail.com>
2022-01-16 06:58:10 +0530

    Fix responsiveness
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import AuthContext from '../../../store/auth-context';
 import Classes from './Dashboard.module.css';
 
@@ -8,17 +8,17 @@ function Dashboard() {
   const [userData, setUserData] = useState({
     name: 'John Doe',
     email: 'foo@foo.com',
-    college: "Blah institute of Blah Technology",
-    phone: "XXXXXXXXXX",
-    year: "2",
-    instaHandle: "_blah_",
+    college: 'Blah institute of Blah Technology',
+    phone: 'XXXXXXXXXX',
+    year: '2',
+    instaHandle: '_blah_',
     userType: '0'
   });
   const getUserType = (props) => {
     const token = sessionStorage.getItem('tokenID');
     // ADD API CALL HERE TO DETERMINE THE TYPE OF USER
 
-    return 2;
+    return 0;
     // Return 0/1/2 depending on the user type;
   };
 
@@ -35,9 +35,27 @@ function Dashboard() {
   return (
     <div>
       <div className={Classes.container}>
-        {/* <h1>Dashboard</h1> */}
+        <h1>Dashboard</h1>
+        <div className={`${Classes.section_accessible}`}>
+          {val == 0 && (
+            <Fragment>
+              <h1>
+                <a href="#">Events</a>
+              </h1>
+              <br />
+              <h1>
+                <a href="#">Merchandise</a>
+              </h1>
+            </Fragment>
+          )}
+          {val == 1 && (
+            <h1>
+              <a href="#">Events</a>
+            </h1>
+          )}
+        </div>
 
-        <div class={Classes.sidenav}>
+        {/* <div class={`${Classes.sidenav} ${Classes.column_1}`}>
           <div class={Classes.profile}>
             <div class={Classes.name}>              
               {userData.name}
@@ -70,17 +88,11 @@ function Dashboard() {
               </div>
             </div>
           )}
-          {/* {val == 2 && (
-            <div>
-              <h1>Congratulations! You get nothing by being a Campus Ambassador!</h1>
-            </div>
-          )} */}
-        </div>
 
-
+        </div> */}
       </div>
 
-      <div className={Classes.main}>
+      <div className={`${Classes.main} ${Classes.column_2}`}>
         <h2>User Information</h2>
         <div className={Classes.card}>
           <div className={Classes.card_body}>
@@ -88,34 +100,53 @@ function Dashboard() {
             <table>
               <tbody>
                 <tr>
-                  <td><strong>Name</strong></td>
+                  <td>
+                    <strong>Name</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.name}</td>
                 </tr>
                 <tr>
-                  <td><strong>Email</strong></td>
+                  <td>
+                    <strong>Email</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.email}</td>
                 </tr>
                 <tr>
-                  <td><strong>College/University</strong></td>
+                  <td>
+                    <strong>College/University</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.college}</td>
                 </tr>
                 <tr>
-                  <td><strong>Phone</strong></td>
+                  <td>
+                    <strong>Phone</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.phone}</td>
                 </tr>
                 <tr>
-                  <td><strong>Year of Study</strong></td>
+                  <td>
+                    <strong>Year of Study</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.year}</td>
                 </tr>
                 <tr>
-                  <td><strong>Instagram Handle</strong></td>
+                  <td>
+                    <strong>Instagram Handle</strong>
+                  </td>
                   <td>:</td>
                   <td>{userData.instaHandle}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <strong>User Type</strong>
+                  </td>
+                  <td>:</td>
+                  <td>{userData.userType == 2 ? 'Campus Ambassador' : 'Participant'}</td>
                 </tr>
               </tbody>
             </table>
