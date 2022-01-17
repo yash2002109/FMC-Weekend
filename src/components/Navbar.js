6c80318116408caf6e8f25536bf8260edd93734e
yash2002109 <yash2002109@gmail.com>
2022-01-17 15:24:39 +0530

    Fix navbar logo relocation bug
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className="nav-item">
+            <li className="nav-item first_item">
               <Link to="/events" className="nav-links" onClick={closeMobileMenu}>
                 EVENTS
               </Link>
