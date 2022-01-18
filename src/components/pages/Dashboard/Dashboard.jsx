97fd97285b4500e265e949c654f1cbde0b2b6d58
yash2002109 <yash2002109@gmail.com>
2022-01-18 13:27:17 +0530

    Add call to fetch dashboard data
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,114 +1,144 @@
 import { Fragment, useContext, useEffect, useState } from 'react';
 import AuthContext from '../../../store/auth-context';
+import Loading from '../../Loading';
 import Classes from './Dashboard.module.css';
 
 function Dashboard() {
-
+  const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState({
     name: 'John Doe',
     email: 'foo@foo.com',
-    college: 'Blah institute of Blah Technology',
+    college: 'Foo',
     phone: 'XXXXXXXXXX',
-    year: '2',
+    year: '0',
     instaHandle: '_blah_',
-    userType: '0'
+    userType: '-1'
   });
-  const getUserData = () => {
-    const token = sessionStorage.getItem('tokenID');
-    // ADD API CALL HERE TO GET THE RELEVANT DETAILS OF THE USER
-  };
 
   useEffect(() => {
-    // When the  page loads fetch userdata
-    // setUserData(getUserData())
-    
+    const isTokenValid = async () => {
+      setIsLoading(true);
+      const token = sessionStorage.getItem('tokenID');
+      try {
+        const res = await fetch('/api/verify-token', {
+          method: 'POST',
+          body: JSON.stringify({
+            token: token
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await res.json();
+
+        // data has message : 'success' if valid and 'invalid' else
+        // on valid, data also has user.email, user.name, user.isNewUser, user.role
+        if (data.message === 'success') {
+          console.log(data);
+          setUserData(data.user);
+        }
+      } catch {
+        alert('Error with authentication, login again');
+      }
+
+      setIsLoading(false);
+    };
+    isTokenValid();
+
+    // console.log(isTokenValid());
   }, []);
-
   return (
+    // <Loading />
     <div>
-      <div className={Classes.container}>
-        <h1>Dashboard</h1>
-        <div className={`${Classes.section_accessible}`}>
-          {userData.userType == 0 && (
-            <Fragment>
-              <h1>
-                <a href="#">Events</a>
-              </h1>
-              <br />
-              <h1>
-                <a href="#">Merchandise</a>
-              </h1>
-            </Fragment>
-          )}
-          {userData.userType == 1 && (
-            <h1>
-              <a href="#">Events</a>
-            </h1>
-          )}
-        </div>
-      </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <div className={Classes.container}>
+            <h1>Dashboard</h1>
+            <div className={`${Classes.section_accessible}`}>
+              {userData.userType == 0 && (
+                <Fragment>
+                  <h1>
+                    <a href="#">Events</a>
+                  </h1>
+                  <br />
+                  <h1>
+                    <a href="#">Merchandise</a>
+                  </h1>
+                </Fragment>
+              )}
+              {userData.userType == 1 && (
+                <h1>
+                  <a href="#">Events</a>
+                </h1>
+              )}
+            </div>
+          </div>
 
-      <div className={`${Classes.main} ${Classes.column_2}`}>
-        <h2>User Information</h2>
-        <div className={Classes.card}>
-          <div className={Classes.card_body}>
-            {/* <i class="fa fa-pen fa-xs edit"></i> */}
-            <table>
-              <tbody>
-                <tr>
-                  <td>
-                    <strong>Name</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.name}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Email</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.email}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>College/University</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.college}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Phone</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.phone}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Year of Study</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.year}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Instagram Handle</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.instaHandle}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>User Type</strong>
-                  </td>
-                  <td>:</td>
-                  <td>{userData.userType == 2 ? 'Campus Ambassador' : 'Participant'}</td>
-                </tr>
-              </tbody>
-            </table>
+          <div className={`${Classes.main} ${Classes.column_2}`}>
+            <h2>User Information</h2>
+            <div className={Classes.card}>
+              <div className={Classes.card_body}>
+                {/* <i class="fa fa-pen fa-xs edit"></i> */}
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <strong>Name</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.name}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>Email</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.email}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>College/University</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.college}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>Phone</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.phone}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>Year of Study</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.year}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>Instagram Handle</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.instaHandle}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong>User Type</strong>
+                      </td>
+                      <td>:</td>
+                      <td>{userData.userType == 2 ? 'Campus Ambassador' : 'Participant'}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
